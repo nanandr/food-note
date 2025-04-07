d2799363de6c9681e48192ebac81694a3240ae0b
Han <59479453+recta-rrae@users.noreply.github.com>
2025-04-07 23:12:19 +0700

    initial commit

---
+++ main.cpp
@@ -0,0 +1,18 @@
+/* 
+UTS SDA KELOMPOK 4 
+Aplikasi Manajemen Resep Makanan (Food Note)
+
+1. Nandana Rafi Ardika (2404158)
+2. Hilmi Mithwa Ramadhan (2404042)
+3. Febriansyah Nugraha (2408163)
+4. Raden Hamizan Rizky Kusuma (2403657)
+5. Raihan Fauzi Rakhman (2403872)
+*/
+
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+
+int main() {
+
+}
