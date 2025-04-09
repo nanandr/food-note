5a3f3764e5ae78c43f378e124581fba503e15f46
HilmiMithwa <hilmimithwa@gmail.com>
2025-04-09 22:05:14 +0700

    menambahkan if else pada create recipe

--- create_recipe.cpp
+++ create_recipe.cpp
@@ -1,8 +1,51 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 using namespace std;
 
 
-int main () {
-    
+struct recipe_makanan {
+    string nama_makanan;
+    string alat_bahan;
+    string langkah_pembuatan;
+};
+
+void tambah_recipe_makanan()
+{
+
+};
+
+void lihat_recipe_makanan()
+{
+
+};
+
+
+int main () 
+{
+    int choice;
+    while (true) {
+    cout << " ---------- Buat Resep Makanan ---------- " << endl;
+    cout << "1. Tambah resep makanan" << endl;
+    cout << "2. Lihat semua resep makanan" << endl;
+    cout << "3. Keluar" << endl;
+    cout << "Pilih menu: " << endl;
+    cin >> choice;
+
+    if (choice == 1) {
+        tambah_recipe_makanan();
+    } else if (choice == 2) {
+        lihat_recipe_makanan();
+    } else if (choice == 3) {
+        break;
+    }
+}
 
 }
+
+
+
+
+
+
+
