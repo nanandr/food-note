ce6a0f80925562eac3b23a7f69cceb60968b1b82
hamizanraden <hamizanraden@gmail.com>
2025-04-15 14:09:04 +0700

    membuat kode looping

--- delete_recipe.cpp
+++ delete_recipe.cpp
@@ -3,13 +3,15 @@
 #include <cstring>
 using namespace std;
 
-struct recipe_makanan {
+struct recipe_makanan 
+{
     string nama_hidangan;
     string bahan_bahan;
     string cara_memasak;
 };
 
-int main() {
+int main() 
+{
     FILE* file = fopen("database/recipe.csv", "r");
     if (!file) {
         cout << "File tidak ditemukan!" << endl;
@@ -19,10 +21,8 @@ int main() {
     recipe_makanan daftar[100];
     int jumlah = 0;
 
-    // Gunakan char[] untuk buffer sementara
     char buffer_nama[100], buffer_bahan[200], buffer_cara[300];
 
-    // Baca file dan masukkan ke struct dengan string
     while (fscanf(file, " %99[^,], %199[^,], %[^\n]\n", 
         buffer_nama, buffer_bahan, buffer_cara) == 3) {
 
@@ -33,32 +33,47 @@ int main() {
     }
     fclose(file);
 
-    // Tampilkan data
     cout << "======================\n";
     cout << "Daftar Resep\n";
     cout << "======================\n";
-    for (int i = 0; i < jumlah; i++) {
+    for (int i = 0; i < jumlah; i++) 
+    {
         cout << i + 1 << ". " << daftar[i].nama_hidangan << endl;
     }
 
-    // Pilih data yang akan dihapus
     int hapus;
-    cout << "Masukkan nomor resep yang ingin dihapus: ";
-    cin >> hapus;
+    while (true)
+    {
+        cout << "Masukkan nomor resep yang ingin dihapus: ";
+        cin >> hapus;
 
-    if (hapus < 1 || hapus > jumlah) {
-        cout << "Nomor tidak valid!" << endl;
-        return 1;
-    }
+        if (hapus >= 1 && hapus <= jumlah) {
+            break; 
+        } 
+        else 
+        {
+            cout << "Nomor tidak valid! Silakan coba lagi.\n";
 
-    // Tulis ulang ke file, kecuali data yang dihapus
+            cout << "======================\n";
+            cout << "Daftar Resep\n";
+            cout << "======================\n";
+
+            for (int i = 0; i < jumlah; i++) 
+            {
+                cout << i + 1 << ". " << daftar[i].nama_hidangan << endl;
+            }
+        }
+    }  
+
     file = fopen("database/recipe.csv", "w");
-    if (!file) {
+    if (!file) 
+    {
         cout << "Gagal membuka file untuk menulis!" << endl;
         return 1;
     }
 
-    for (int i = 0; i < jumlah; i++) {
+    for (int i = 0; i < jumlah; i++) 
+    {
         if (i != hapus - 1) {
             fprintf(file, "%s, %s, %s\n",
                 daftar[i].nama_hidangan.c_str(),
