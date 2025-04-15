d93aeb647fa0d66da2e9112c47dd6f82a0a9b07e
nanand.r <rafirenan12@gmail.com>
2025-04-15 12:31:02 +0700

    buat resep baru main

--- main.cpp
+++ main.cpp
@@ -10,10 +10,99 @@ Aplikasi Manajemen Resep Makanan (Food Note)
 */
 
 #include <stdio.h>
+#include <vector>
 #include <iostream>
 using namespace std;
 
-int main() {
-    
+struct Ingredient {
+    string name;
+    string amount;
+    Ingredient *next;
+};
 
+struct Recipe {
+    string name;
+    Ingredient* ingredient;
+    vector<string> steps;
+};
+
+int main() {
+    // navigation value
+    int nav = 0;
+
+    // main loop
+    while (true) {
+    // title
+    // title();
+
+    // navigation
+    // int nav = navigate_to({"Buat Resep Baru", "Daftar Resep", "Cari Resep", "Keluar"});
+    
+    // Buat Resep Baru
+        if (nav == 1) {
+            Recipe recipe;
+
+            Ingredient* tail = nullptr;
+
+            int i = 0;
+            int j = 0;
+            string name, amount, step;
+
+            // header("Buat Resep Baru");
+            cout << "Nama Hidangan: ";
+            cin >> recipe.name;
+            // cout << border_thin;
+            
+            cout << "Bahan-bahan: \n";
+            // input loop, type 0 to finish
+            while (true) {
+                cout << "[" << i+1 << "] Nama Bahan: ";
+                cin >> name;
+                if (name == "0") break;
+
+                cout << "     Jumlah: ";
+                cin >> amount;
+                
+                Ingredient* ingredient = new Ingredient;
+                ingredient->name = name;
+                ingredient->amount = amount;
+                ingredient->next = nullptr;
+
+                if (recipe.ingredient == nullptr) {
+                    recipe.ingredient = ingredient;
+                }
+                else {
+                    tail->next = ingredient;
+                }
+                tail = ingredient;
+                i++;
+            }
+
+            // Cara memasak:
+            // border_thin();
+            cout << "Cara memasak: \n";
+            // steps loop
+            while (true) {
+                cout << "[" << i+1 << "] ";
+                cin >> step;
+                if (step == "0") break;
+
+                // put to steps vector
+            }
+        }
+        // Daftar Resep
+        else if (nav == 2) {
+            // header("Daftar Resep");
+            
+        } 
+        // Cari Resep
+        else if (nav == 3) {
+            // header("Cari Resep");
+            
+        }
+        // Keluar
+        else if (nav == 0) {
+            break;
+        }
+    }
 }
