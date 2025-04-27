2693bfc2cb736e1454a9f9924c562381619a2a84
hanz-fr <raihanf787@gmail.com>
2025-04-27 23:18:04 +0700

    feat: try to add registration

--- main.cpp
+++ main.cpp
@@ -190,6 +190,45 @@ int login_menu()
     return 0;
 }
 
+int register_menu()
+{
+    string line;
+    string inp_surel_pengguna;
+    string inp_kata_sandi;
+    string inp_nama_pengguna;
+    char del = ','; // delimiter pemisah string
+
+    cout << "\n";
+    cout << "========================" << endl;
+    cout << "|                      |" << endl;
+    cout << "|  Daftar Pengguna     |" << endl;
+    cout << "|  Baru                |" << endl;
+    cout << "|                      |" << endl;
+    cout << "========================" << endl;
+    cout << "Masukkan surel pengguna (\"kembali\" untuk kembali ke menu awal)" << endl;
+    cout << "> ";
+    cin >> inp_surel_pengguna;
+
+    // jadiin lowercase buat input surel
+    string surel_pengguna_lower = inp_surel_pengguna;
+    transform(surel_pengguna_lower.begin(), surel_pengguna_lower.end(), surel_pengguna_lower.begin(), ::tolower);
+
+    // kalau inputannya "kembali" pas input surel, return -1
+    int compareInputToExit = surel_pengguna_lower.compare("kembali");
+    if (compareInputToExit == 0) {
+        return -1;
+    }
+
+    cout << "Masukkan nama pengguna" << endl;
+    cout << "> ";
+    getline(cin, inp_nama_pengguna);
+    
+    cout << "Buat kata sandi baru" << endl;
+    cout << "> ";
+    cin >> inp_kata_sandi;
+
+}
+
 void show_main_menu()
 {
     cout << "\n";
